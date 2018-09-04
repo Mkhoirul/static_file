8d3ad70ae369c3a2767f41d1165f25513de86f3b
mkhoirul <mkhoirulanwar41@gmail.com>
2018-09-04 14:26:43 +0700

    menambahkan template about

--- about/views.py
+++ about/views.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+	context = {
+		'title':'About',
+		'heading':'About',
+		'subheading':'tentang kelas about',
+	}
+	return render(request,'about/index.html',context)
